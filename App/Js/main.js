2358139e2b8d67ac4d983cff59fc3893744d57a5
LittaPanda <ing.karla.hernandez@gmail.com>
2013-01-17 12:31:56 -0600

    Verificador de cache agregado

--- App/Js/main.js
+++ App/Js/main.js
@@ -1,1 +1,11 @@
-
+// Verificación de nuevos cambios en el servidor
+window.addEventListener('load', function(e) {
+	 window.applicationCache.addEventListener('updateready', function(e) {
+		if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+		  window.applicationCache.swapCache();
+		  window.location.reload();
+		} else {
+		  // El manifiesto no cambio. No hay nada nuevo.
+		}
+	  }, false);
+}, false);
