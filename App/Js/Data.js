30676a412ef333e005f94ea77e632ccb4e6cba58
RyoAkiyama01 <www.power_of_digimon01@hotmail.com>
2013-01-18 11:39:47 -0600

    Base de Datos
    Aqui se encuentra la base de datos de los carros

---
+++ App/Js/Data.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var CarsInformation =  
+	[
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Blue","Price":"250000","Version":"De luxe"},
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Blue","Price":"180000","Version":"Standar"},
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Red","Price":"220000","Version":"De luxe"},
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Red","Price":"1900000","Version":"Premium"},
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Red","Price":"150000","Version":"Standar"},
+	{"Brand":"Nissan", "Model":"Leaf", "Color":"Green","Price":"180000","Version":"Premium"},
+	
+	{"Brand":"Nissan", "Model":"Stone", "Color":"Light Blue","Price":"160000","Version":"Standar"},
+	{"Brand":"Nissan", "Model":"Stone", "Color":"Blue","Price":"160000","Version":"Standar"},
+	{"Brand":"Nissan", "Model":"Stone", "Color":"Light Red","Price":"220000","Version":"De luxe"},
+	{"Brand":"Nissan", "Model":"Stone", "Color":"Light Red","Price":"200000","Version":"Premium"},
+	];
