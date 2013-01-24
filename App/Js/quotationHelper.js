011a8679e3910a483676e31a04611f0890beca0d
LittaPanda <ing.karla.hernandez@gmail.com>
2013-01-24 16:52:50 -0600

    Report

---
+++ App/Js/quotationHelper.js
@@ -0,0 +1,30 @@
+function quotation()
+{
+	var data = {Residue:450000.0,Months:12,AnualRate:8.5,payPeriod:1}
+	var MonthPay = [];
+	var residue = data.Residue;
+	var months = data.Months;
+	var payPeriod = (data.payPeriod/12) * data.AnualRate;
+	var amortizedCapital =0.0;
+	MonthPay.push({"numberOfPay":0, "pay":0, "interestAmount":0,"capitalAmount":0, "balance":residue,"amortizedCapital":amortizedCapital });	
+	var powVale =Math.pow( 1 + (payPeriod/100),0-months);
+	var pay = (residue * payPeriod) / (100*(1-powVale));
+	var count = 1;		 
+	do
+	{
+		
+		var newInterest = (residue * payPeriod)/100;	
+		var newCapital = pay - newInterest;
+		residue = residue.toFixed(6) - newCapital.toFixed(6);
+		amortizedCapital= newCapital + amortizedCapital;   	  
+		MonthPay.push({"numberOfPay":count, 
+				 "pay":pay.toFixed(2), 
+				 "interestAmount":newInterest.toFixed(2),
+				 "capitalAmount":newCapital.toFixed(2), 
+				 "balance":residue.toFixed(2),
+				 "amortizedCapital":amortizedCapital.toFixed(2) });
+		count++;
+	}
+	while(residue.toFixed(1)!= 0.0);
+	return MonthPay;
+}
