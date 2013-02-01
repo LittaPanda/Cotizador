97565e093b33045b9b8b59957b3c35fe57e5bd23
LittaPanda <ing.karla.hernandez@gmail.com>
2013-02-01 09:22:30 -0600

    periodo de pago

--- App/Js/navigationManagement.js
+++ App/Js/navigationManagement.js
@@ -119,31 +119,33 @@ function onChangeDetailbyColor(brand, model, version, color)
 
 function onChangePeriod()
 {
-	var newPeriod = $('#PeriodPay').val();
-	var serializedData = $('#form_customer').serializeArray();
-	var customerData = {firstName : serializedData[0].value,
-						lastName1 : serializedData[1].value,
-						lastName2 : serializedData[2].value,
-						rfc : serializedData[3].value,
-						address : serializedData[4].value ,
-						neighborhood : serializedData[5].value ,
-						city : serializedData[6].value ,
-						stateSelector : serializedData[7].value ,
-						country : serializedData[8].value ,
-						zipCode : serializedData[9].value ,
-						phone : serializedData[10].value ,
-						email : serializedData[11].value,
-						period : newPeriod					
-		};
-	var carData = { 	SelectedBrand : serializedData[12].value ,
-						SelectedModel : serializedData[13].value ,
-						SelectedVersion : serializedData[14].value ,
-						SelectedColor : serializedData[15].value ,
-						SelectedDescription : serializedData[16].value,
-						SelectedPrice : parseFloat(serializedData[17].value) 
-		};
-	var data = {Residue:carData.SelectedPrice,Months:newPeriod,AnualRate:8.5,payPeriod:1};						
-	renderReport(customerData,carData,data);
+	var t = setTimeout(function(){
+		var newPeriod = $('#PeriodPay').val();
+		var serializedData = $('#form_customer').serializeArray();
+		var customerData = {firstName : serializedData[0].value,
+							lastName1 : serializedData[1].value,
+							lastName2 : serializedData[2].value,
+							rfc : serializedData[3].value,
+							address : serializedData[4].value ,
+							neighborhood : serializedData[5].value ,
+							city : serializedData[6].value ,
+							stateSelector : serializedData[7].value ,
+							country : serializedData[8].value ,
+							zipCode : serializedData[9].value ,
+							phone : serializedData[10].value ,
+							email : serializedData[11].value,
+							period : newPeriod					
+			};
+		var carData = { 	SelectedBrand : serializedData[12].value ,
+							SelectedModel : serializedData[13].value ,
+							SelectedVersion : serializedData[14].value ,
+							SelectedColor : serializedData[15].value ,
+							SelectedDescription : serializedData[16].value,
+							SelectedPrice : parseFloat(serializedData[17].value) 
+			};
+		var data = {Residue:carData.SelectedPrice,Months:newPeriod,AnualRate:8.5,payPeriod:1};						
+		renderReport(customerData,carData,data);
+		},3000);	
 }
 
 $(document).bind( "pagebeforechange", function( e, data ) {
